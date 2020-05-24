d7e6fe233b0baba9ead6d8549df77066ffd43bf2
Roman <romanmendel777@gmail.com>
2020-05-25 00:59:32 +0300

    new project

---
+++ my-app/src/components/Profile/MyPosts/Post/Post.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import s from './Post.module.css';
+
+type propsType = {
+  message: string;
+}
+
+const Post = (props: propsType) => {
+
+  return (
+    <div className={s.item}>
+      <img src='https://movies4maniacs.liberty.me/wp-content/uploads/sites/1218/2015/09/avatarsucks.jpg' />
+      {props.message}
+      <div>
+        <span>like</span>
+      </div>
+    </div>
+  )
+}
+
+export default Post;
