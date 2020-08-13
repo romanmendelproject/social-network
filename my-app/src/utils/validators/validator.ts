a44ebb458505354d70bd0afd8409b24560d8f909
romanmendelproject <romanmendel777@gmail.com>
2020-08-14 01:20:53 +0300

    add validator

---
+++ my-app/src/utils/validators/validator.ts
@@ -0,0 +1,10 @@
+export const required = (value: string) => {
+    if (value) return undefined
+    return "Field is required"
+}
+
+export const maxLenghtCreator = (maxLenght: number) => (value: string) => {
+    if (value.length > maxLenght) return `Max lenght is ${maxLenght} symbols`
+    return undefined
+}
+
