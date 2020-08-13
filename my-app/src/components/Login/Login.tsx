b151a0315399065a53cc0b66ddab4ac686fe6cab
romanmendelproject <romanmendel777@gmail.com>
2020-08-14 01:20:31 +0300

    add login

--- my-app/src/components/Login/Login.tsx
+++ my-app/src/components/Login/Login.tsx
@@ -1,6 +1,39 @@
 import React from 'react';
+import { reduxForm, Field } from 'redux-form';
+import { Input } from '../common/FormsControls/FormsControls';
+import { required } from '../../utils/validators/validator';
 
-const Login = (props:any) => {
-    return <h1>Login</h1>
+
+const LoginForm = (props: any) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field placeholder={"Login"} name={"login"} component={Input} validate={[required]}/>
+            </div>
+            <div>
+                <Field placeholder={"Password"} name={"password"} component={Input} validate={[required]}/>
+            </div>
+            <div>
+                <Field component={"input"} name={"rememberMe"} type={Input} /> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
 }
+
+const LoginReduxForm = reduxForm({ form: 'login' })(LoginForm)
+
+const Login = (props: any) => {
+    const onSubmit = (formData:any) => {
+
+    }
+    return <div>
+        <h1>Login</h1>
+        <LoginReduxForm onSubmit={onSubmit}/>
+
+    </div>
+}
+
 export default Login
